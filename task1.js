7deb20009aaf035dc6e9b0d8926487bce3d03f80
tribunalo <143053954+tribunalo@users.noreply.github.com>
2024-03-08 20:36:10 +0800

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,29 @@
+// 1. Declaring the  variables of different data types
+let name = "Nikka Jane tribunalo";  // String
+let age = 24;        // Number (integer)
+let cute = true;     // Boolean
+
+// 2. Performing  arithmetic operations
+let number1=parseInt(prompt("Enter first number: "));
+let number2=parseInt(prompt("Enter second number: "));
+
+let sum = number1 + number2;
+let difference = number1 - number2;
+let product = number1 * number2;
+let quotient = number1 / number2;
+
+// 3. Implementing basic conditional statements
+if (age > 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not an adult.");
+}
+
+// 4. Utilizing console.log() to output results
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Nikka Jane:", Gorgeous);
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
